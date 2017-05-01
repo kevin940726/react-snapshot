6ad86f99dee738421dcbec039f24c3391758e3d0
Kai Hao <kevin830726@gmail.com>
2017-05-01 12:26:23 +0800

    ✨ Feat: add free port selection

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,7 @@ import url from 'url'
 import Server from './Server'
 import Crawler from './Crawler'
 import Writer from './Writer'
+const getPort = require('get-port')
 
 const pkg = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'package.json')))
 const publicPath = pkg.homepage ? url.parse(pkg.homepage).pathname : '/'
@@ -13,16 +14,17 @@ export default () => {
   const writer = new Writer(baseDir)
   writer.move('index.html', '200.html')
 
-  const server = new Server(baseDir, publicPath, 2999)
-  server.start().then(() => {
-    const crawler = new Crawler(`http://localhost:2999${publicPath}`)
-    return crawler.crawl(({ path, html }) => {
+  getPort(2999)
+    .then(port => {
+      const server = new Server(baseDir, publicPath, port)
+      return server.start().then(() => new Crawler(`http://localhost:${port}${publicPath}`))
+    })
+    .then(crawler => crawler.crawl(({ path, html }) => {
       const filename = path === publicPath ?
         'index.html' :
         `${path}${path.endsWith('/') ? 'index' : ''}.html`
       console.log(`✏️   Saving ${path} as ${filename}`)
       writer.write(filename, html)
-    })
-
-  }).then(() => server.stop(), err => console.log(err))
+    }))
+    .then(() => server.stop(), err => console.error(err))
 }
